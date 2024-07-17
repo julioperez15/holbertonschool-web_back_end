72ada30d1c44326cebf613068dd37f0898530dd4
Julio <julio.1515.pr@gmail.com>
2024-07-17 12:28:16 -0400

    task 0 fix

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,16 +1,10 @@
-  export const taskFirst = () => {
-    const task = 'I prefer const when I can.';
-    return task;
-  };
-  
-  export const getLast = () => {
-    return ' is okay';
-  };
-  
-  export const taskNext = () => {
-    let combination = 'But sometimes let';
-    combination += getLast();
-    
-    return combination;
-  };
-  
+export const taskFirst = () => 'I prefer const when I can.';
+
+export const getLast = () => ' is okay';
+
+export const taskNext = () => {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+};

bf9980d3580d439f97dedce46fdc7cb8705db07b
Julio <julio.1515.pr@gmail.com>
2024-07-18 21:05:13 -0400

    task 10 fix

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,17 +1,11 @@
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
-class Car {
+export default class Car {
     constructor(brand, motor, color) {
-        this[_brand] = brand;
-        this[_motor] = motor;
-        this[_color] = color;
+      this._brand = brand;
+      this._motor = motor;
+      this._color = color;
     }
-
+  
     cloneCar() {
-        return new Car(this[_brand], this[_motor], this[_color]);
+      return new this.constructor();
     }
-}
-
-export default Car;
+  }

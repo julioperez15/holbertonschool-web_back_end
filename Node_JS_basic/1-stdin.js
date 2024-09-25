4bdecb7867455aeb86ec1a4e570f0bdc65abdef5
Julio <julio.1515.pr@gmail.com>
2024-09-24 20:02:49 -0400

    adding all tasks

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,14 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input !== null) {
+    process.stdout.write(`Your name is: ${input}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
+});
